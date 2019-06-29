dfc7d590e293cb743e18499920b8a0250b6626e2
fistaco <fiske_schijlen@hotmail.com>
2019-06-30 00:47:09 +0200

    Define helper functions to convert labels to and from numerical representations

--- helpers.py
+++ helpers.py
@@ -3,6 +3,18 @@ import pickle
 import pandas as pd
 
 
+# Define dictionaries to map labels to numeric values and the other way around
+label_strings = [
+    'blurry-nothing', 'colon-clear', 'dyed-lifted-polyps',
+    'dyed-resection-margins', 'esophagitis', 'instruments','normal-cecum',
+    'normal-pylorus', 'normal-z-line', 'out-of-patient', 'polyps',
+    'retroflex-rectum', 'retroflex-stomach', 'stool-inclusions',
+    'stool-plenty', 'ulcerative-colitis'
+]
+label_str_to_num = dict(zip(label_strings, range(16)))
+label_num_to_str = dict(zip(range(16), label_strings))
+
+
 def load_feature_df_and_relevant_data_from_pickles():
     feature_df = pd.read_pickle("./feature_df.pickle")
 
@@ -13,3 +25,11 @@ def load_feature_df_and_relevant_data_from_pickles():
     data_pickle_file.close()
 
     return (feature_df, labels, index_to_feature_path, index_to_img_path)
+
+
+def convert_label_str_to_num(label_str):
+    return label_str_to_num[label_str]
+
+
+def convert_label_num_to_str(label_num):
+    return label_num_to_str[label_num]
