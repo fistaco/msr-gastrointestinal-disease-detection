2d9c703a1f97f37e3652e4d3f244c45557780184
fistaco <fiske_schijlen@hotmail.com>
2019-06-30 01:35:07 +0200

    Try several classifiers and leave in the most effective one

--- classify.py
+++ classify.py
@@ -1,30 +1,42 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import SGDClassifier, LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, \
+    precision_score, recall_score, roc_curve, roc_auc_score
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import BernoulliNB
+from sklearn.svm import LinearSVC
 
-from helpers import load_feature_df_and_relevant_data_from_pickles
+from helpers import *
 
 # Load our pre-processed data from our pickle file
 (feature_df, labels, df_index_to_feature_path, df_index_to_img_path) = \
     load_feature_df_and_relevant_data_from_pickles()
 
+# Convert labels to numeric values for classification
+labels = np.array([convert_label_str_to_num(label) for label in labels])
+
 # Split in train and test data
 x_train, x_test, y_train, y_test = \
     train_test_split(feature_df, labels, test_size=0.25)
 
-# Bernoulli Naive Bayes classification
-bernoulli_clf = BernoulliNB()
-bernoulli_clf.fit(x_train, y_train)
+# Define candidate classifiers to test
+clfs = {
+    "SGD": SGDClassifier(random_state=7),
+    "LogReg": LogisticRegression(multi_class="ovr", solver="lbfgs"),
+    "LinearSVC": LinearSVC(random_state=7),
+    "RandForest": RandomForestClassifier(random_state=7)
+}
 
-y_pred = bernoulli_clf.predict(x_test)
-acc = accuracy_score(y_test, y_pred)
+# Train and evaluate each classifier
+for (id, clf) in clfs.items():
+    print(f"Training {id} classifier!")
+    clf.fit(x_train, y_train)
 
-# Multi-class logistic regression classification
-logreg_clf = LogisticRegression(multi_class="ovr")
-logreg_clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
 
-y_pred = logreg_clf.predict(x_test)
-acc = accuracy_score(y_test, y_pred)
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred, average="micro")
+    recall = recall_score(y_test, y_pred, average="micro")
+    f1 = f1_score(y_test, y_pred, average="micro")
+    print(f"{id}: accuracy={acc}, precision={prec}, recall={recall}, f1={f1}")
